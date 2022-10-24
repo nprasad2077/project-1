c8c71cf9e26eba1ce7af61c772858d4d74966aea
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-10-24 11:32:47 -0500

    flip

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll('.memory-card');    //Select all card elements
 
 function flipCard() {
+    this.classList.toggle('flip')
     console.log('clicked');
     console.log(this);
 }
 
-cards.forEach(cards => cards.addEventListener('click', flipCard))
+cards.forEach(cards => cards.addEventListener('click', flipCard))   //Add Event listeners to card elements. When clicked, execute flip function.
