a5122a0e675056cbd005c5f124bfacb0f69434ce
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-10-24 12:49:12 -0500

    data-framework

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const cards = document.querySelectorAll('.memory-card');    //Select all card elements
+let hasFlipped = false;
+let firstCard, secondCard;
 
 function flipCard() {
     this.classList.toggle('flip')
-    console.log('clicked');
-    console.log(this);
+    // console.log('clicked');
+    // console.log(this);
+
+    if (!hasFlipped) {      // First Click
+        hasFlipped = true;
+        firstCard = this;
+        // console.log(hasFlipped, firstCard);
+    } else {                // Second Click
+        hasFlipped = false;
+        secondCard = this;
+        // console.log(firstCard, secondCard);
+
+    }
 }
 
 cards.forEach(cards => cards.addEventListener('click', flipCard))   //Add Event listeners to card elements. When clicked, execute flip function.
